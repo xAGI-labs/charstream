d4d2c54743ee57cbf31d2e8c5e8e40689b498892
comethrusws <basabjha@naver.com>
2025-03-29 15:16:03 +0545

    Commit #9.5: /ghiblify ad in Discover

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next"
 import DiscoverPageContent from "@/components/discover/discover-page-content"
 import ThemeContextProvider from "@/context/theme-context"
+import { GhiblifyAdSection } from "@/components/ghiblify-ad-section"
 
 export const metadata: Metadata = {
   title: "Discover Characters - Chatstream",
@@ -10,6 +11,9 @@ export const metadata: Metadata = {
 export default function DiscoverPage() {
   return (
     <ThemeContextProvider>
+    <section className="mx-4 md:mx-10">
+      <GhiblifyAdSection />
+    </section>
     <div className="flex h-screen">
         <DiscoverPageContent />
     </div>
